bc4cad2037b503995e42d42d4122a9eb3b48e897
Beth Ruff <beth.ruff2@gmail.com>
2020-02-02 07:56:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ hash = {:blake => 500, :ashley => 2, :adam => 1}
 def key_for_min_value(name_hash)
  smallest_key = nil
   tiniest_value = nil
-  hash.each do |key, value|
+  name_hash.each do |key, value|
     if tiniest_value == nil || value < tiniest_value
       tiniest_value = value
       smallest_key = key
